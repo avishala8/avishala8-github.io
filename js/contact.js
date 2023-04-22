700863c8640de73f9c6f97182e2af41a23497546
avishala8 <abhishekvishala@gmail.com>
2023-04-22 14:45:13 +0530

    Initial commit

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form');
+const thankYouMessage = document.querySelector('#thank-you-message');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  thankYouMessage.classList.add('show');
+  setTimeout(() => form.submit(), 2000);
+});
